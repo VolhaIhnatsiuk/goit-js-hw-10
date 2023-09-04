a71bf3feee3dff644ebcca86c0e00e6801f9326e
przystanwrozmyslaniu <iov220291@gmail.com>
2023-09-04 18:34:24 +0200

    change after check

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,87 +1,26 @@
-import SlimSelect from 'slim-select'
-import 'slim-select/dist/slimselect.css';
-const PRIVATE_KEY = 'live_pnWdDrUfXQvcd9jZzEtMbuL8DN7q46DVKndy4HH3T6ZA47LhtQdktp9ITo2p21Os'
-const searchOptions = {
-    headers: {'x-api-key' : PRIVATE_KEY },
-}
+import axios from 'axios';
 
-const loader = document.querySelector(".loader")
-const errorDiv = document.querySelector(".error")
-const catDiv = document.querySelector(".cat-info")
-const selection = document.querySelector(".breed-select")
-loader.style.display = "none"
+const BASE_URL = 'https://api.thecatapi.com/v1';
 
-export function fetchBreeds() {
-    loader.style.display = "block" 
-    fetch("https://api.thecatapi.com/v1/breeds")
+function fetchBreeds() {   
+  return axios
+    .get(`${BASE_URL}/breeds`)
     .then(response => {
-        if (!response.ok) {
-            throw new Error(response.statusText)
-         }
-    
-        return response.json()
-        
-        })
-      .then(data => {
-        loader.style.display = "none"
-        data.forEach(element => {
-            const option = `<option value="${element.id}">${element.name}</option>`
-            selection.insertAdjacentHTML("beforeend", option) 
-          
-        });
-
-        new SlimSelect({
-            select: '#cats'
-        })
-
-    })  
-    
-        .catch(error => {
-            loader.style.display = "none"
-            errorDiv.style.color = "red"
-            errorDiv.innerHTML = "Oops! Something went wrong! Try reloading the page!";     
-    })  
-    
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Помилка запиту:', error.message);
+    });
 }
 
-export function addCatInfo(event) {
-    errorDiv.setAttribute("hidden", "")
-    catDiv.style.display = "none"
-    loader.style.display = "block"
-    const breed = event.target.value;
-    fetch(`https://api.thecatapi.com/v1/images/search?breed_ids=${breed}`, searchOptions)
-            
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Oops! Something went wrong! Try reloading the page!")
-            }
-            errorDiv.setAttribute("hidden", "")
-            return response.json()
-        })
-            
-        .then(data => {
-            catDiv.innerHTML = ``
-            const markU = `<div class="text-div"><h1 class="text-div-title">${data[0].breeds[0].name}</h1><p class="text-div-description">${data[0].breeds[0].description}</p><p class="text-div-temperament"><b>Temperament: </b>${data[0].breeds[0].temperament}</p></div>`
-
-            const image = new Image();
-            image.src = data[0].url;
-            image.width =  500;
-            image.height = 500;
-            image.onload = function () {
-                catDiv.innerHTML = markU
-                catDiv.appendChild(image);
-                loader.style.display = "none";
-            }
-
-            catDiv.style.display = "flex"
-        }) 
-            
-        .catch(error => {
-            errorDiv.removeAttribute("hidden")
-            loader.style.display = "none"
-            errorDiv.style.color = "red"
-            errorDiv.innerHTML = "Oops! Something went wrong! Try reloading the page!";
-            catDiv.setAttribute("hidden", "")
-        })
-    
+function fetchCatByBreed(breedId) {
+  return axios
+    .get(`${BASE_URL}/images/search?breed_ids=${breedId}`)
+    .then(response => {
+      return response.data;
+    })
+    .catch(error => {
+      throw new Error('Помилка запиту:', error.message);
+    });
 }
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,63 @@
-import axios from "axios";
-import {fetchBreeds, addCatInfo} from "./cat-api"
+import axios from 'axios';
+import { fetchBreeds, fetchCatByBreed } from './cat-api';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-axios.defaults.headers.common["x-api-key"] = "live_pnWdDrUfXQvcd9jZzEtMbuL8DN7q46DVKndy4HH3T6ZA47LhtQdktp9ITo2p21Os";
+axios.defaults.headers.common['x-api-key'] = 'live_pnWdDrUfXQvcd9jZzEtMbuL8DN7q46DVKndy4HH3T6ZA47LhtQdktp9ITo2p21Os';
 
-const selection = document.querySelector(".breed-select")
+const select = document.querySelector('.breed-select');
+const card = document.querySelector('.cat-info');
+const loader = document.querySelector('.loader');
+const error = document.querySelector('.error');
 
-fetchBreeds()
+select.addEventListener('change', onChange);
 
-selection.addEventListener("change", addCatInfo);
+function createCatList() {
+  loader.classList.remove('is-hidden');
+  fetchBreeds()
+    .then(data => {
+      const options = data
+        .map(({ id, name }) => `<option value="${id}">${name}</option>`)
+        .join(' ');
+      select.innerHTML = options;
+      new SlimSelect({
+        select: select,
+      });
+      loader.classList.add('is-hidden');
+      select.classList.remove('is-hidden');
+    })
+    .catch(error => {
+      Notify.failure('Oops! Something went wrong! Try reloading the page!');
+    });
+}
+createCatList();
+
+
+function onChange(evt) {
+  loader.classList.remove('is-hidden');
+  card.classList.add('is-hidden');
+  const selectedBreedId = evt.currentTarget.value;
+  fetchCatByBreed(selectedBreedId)
+    .then(data => {
+      renderMarkupInfo(data);
+      loader.classList.add('is-hidden');
+      card.classList.remove('is-hidden');
+    })
+    .catch(error => {
+      loader.classList.add('is-hidden');
+      Notify.failure('Oops! Something went wrong! Try reloading the page!', {position: 'center-top',width:'400px', fontSize: '18px'});
+    });
+}
+
+function renderMarkupInfo(data) {
+    console.log(data)
+  const { breeds, url } = data[0];
+  const { name, temperament, description } = breeds[0];
+  const beerdCard = `<img class="cat-img" width = "400" src="${url}" alt="${name}">
+    <div class="descr-part">
+  <h2 class="cat-name">${name}</h2>
+  <p class="deskr-text">${description}</p>
+  <p class="cat-temperament"><span class="span-temperament">Temperament:</span> ${temperament}</p></div>`;
+  card.innerHTML = beerdCard;
+}
